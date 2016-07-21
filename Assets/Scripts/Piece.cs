fe4fdc06494d451285da7c68a3e4c171b40816f0
Oguz Huner <oguzcanhuner@gmail.com>
2016-07-21 15:16:40 +0200

    smooth piece movement

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class Piece : BoardEntity {
 	private static Dictionary<Vector3, Piece> pieces = new Dictionary<Vector3, Piece> ();
+	public float movementSpeed;
 
 	public static Piece GetPiece(Vector3 centre){
 		centre = new Vector3 (centre.x, 0, centre.z);
@@ -30,10 +31,23 @@ public class Piece : BoardEntity {
 	}
 
 	public void MoveTo(float x, float z){
+		StartCoroutine( SmoothMove (new Vector3 (x, transform.position.y, z)));
 		Piece.pieces.Remove (this.Centre());
 		Piece.pieces.Add (normalize (x, z), this);
-		transform.position = new Vector3(x, transform.position.y, z);
 
+
+
+	}
+
+	private IEnumerator SmoothMove(Vector3 destination){
+		Vector3 startPosition = this.transform.position;
+
+		while (transform.position != destination) {
+			Vector3 move = Vector3.MoveTowards (transform.position, destination, movementSpeed * Time.deltaTime);
+			transform.position = move;
+			Debug.Log (move);
+			yield return null;
+		}
 	}
 
 	public List<Vector3> possibleMoves(){
